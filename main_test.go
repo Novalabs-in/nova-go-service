681e6ebad3719a21d9335bac8f06bbfc15c71529
Sai Teja  Bandaru <Saitejaroyal2311@gmail.com>
2026-06-06 16:03:39 +0200

    Add unit tests for Go service

---
+++ main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+def testPlaceholder(t *testing.T) {
+    // Basic compilation check
+    if false {
+        t.Errorf("Should not happen")
+    }
+}
